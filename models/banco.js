0393c9672142686a6adb87b6cb559fe2adb17c90
Edward Mevis da Silva <118694026+EdwardMevis@users.noreply.github.com>
2024-07-10 21:44:27 -0300

    Update banco.js

--- models/banco.js
+++ models/banco.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(
-    "test", "root", "", {
+    "padaria", "root", "", {
         host: "localhost",
         dialect: "mysql"
     }
